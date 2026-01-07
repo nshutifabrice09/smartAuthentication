1a1d33508b5f258f3bc6cdc8bd52a1967d780ab6
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-01-07 09:00:30 +0200

    initial commit of the project

---
+++ src/main/java/com/example/smartAuthentication/SmartAuthenticationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAuthenticationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAuthenticationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smartAuthentication/SmartAuthenticationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAuthenticationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
