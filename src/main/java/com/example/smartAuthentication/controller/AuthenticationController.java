f63f529ec1399f175b86540cc2e69b6ad87fbada
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-01-08 18:40:23 +0200

    loginResponse commit

--- src/main/java/com/example/smartAuthentication/controller/AuthenticationController.java
+++ src/main/java/com/example/smartAuthentication/controller/AuthenticationController.java
@@ -1,7 +1,39 @@
 package com.example.smartAuthentication.controller;
 
+import com.example.smartAuthentication.dto.RegisterUserDto;
+import com.example.smartAuthentication.model.User;
+import com.example.smartAuthentication.response.LoginResponse;
+import com.example.smartAuthentication.service.AuthenticationService;
+import com.example.smartAuthentication.service.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@RequestMapping("/auth")
 public class AuthenticationController {
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    private final List<User> users = new ArrayList<>();
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        User registeredUser = authenticationService.signUp(registerUserDto);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse>
 }
